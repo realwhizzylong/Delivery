2303ed5904b5b91b28d276e3e8f3f69733c2d40d
realwhizzylong <therealwhizzy@gmail.com>
2021-04-30 15:40:22 +0100

    remove firebase

--- app/src/main/java/com/example/delivery/activities/LoginActivity.java
+++ app/src/main/java/com/example/delivery/activities/LoginActivity.java
@@ -3,26 +3,14 @@ package com.example.delivery.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 
 import com.example.delivery.R;
 import com.example.delivery.utils.UserUtil;
 import com.example.delivery.views.InputView;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends BaseActivity {
 
     private InputView inputEmail, inputPassword;
-
-    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +25,6 @@ public class LoginActivity extends BaseActivity {
 
         inputEmail = fd(R.id.input_email);
         inputPassword = fd(R.id.input_password);
-
-        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void onLoginClick(View view) {
@@ -49,18 +35,8 @@ public class LoginActivity extends BaseActivity {
             return;
         }
 
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (!task.isSuccessful()) {
-                    Toast.makeText(LoginActivity.this, "Invalid login credentials", Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
